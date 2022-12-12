a2772e8ec39d579285b4791b74218fac4ac392a8
daw2-gil23 <93734349+daw2-gil23@users.noreply.github.com>
2022-12-12 20:35:11 +0100

    Update script.sh
    Añadiendo sudo en el script

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t im3-gil23:latest .
-docker run --name contenidor03-gil23 -p 80:80 -i -t -d im03-gil23
+sudo docker build -t im03b-gil23:latest .
+sudo docker run --name contenidor03b-gil23 -p 80:80 -i -t -d im03b-gil23
